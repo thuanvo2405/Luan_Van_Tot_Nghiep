6159a8048a7ff3d159463651a9bc68ca54e1b956
thuanvo2405 <vothuan2405003@gmail.com>
2025-10-18 09:58:46 +0700

    Set up project

---
+++ src/components/header.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+
+export default function Header() {
+  return (
+    <header>
+      <div>Logo</div>
+      <div>
+        <input type="text" />
+      </div>
+      <Button>Tạo sự kiện</Button>
+      <Button>Username</Button>
+      <div className="relative group">
+        <DropdownMenu>
+          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+  );
+}
